151108a51967bc76ac1d591a423a2eea13c6102c
Anna Thoss <annat@noacs.io>
2023-12-04 10:59:40 +0100

    mm + res

---
+++ functions/select_varaibles.R
@@ -0,0 +1,10 @@
+#' Select Varaibles
+#'
+#' Write a short description of your function here 
+#' @param x Describe argument x here 
+#' @export 
+select_varaibles <- function(x) {
+    ## Replace with the contents of your function
+    y <- x + 1
+    return (y)
+}
